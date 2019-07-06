64a1401d5220f590b0fbeca89b6b7fe0893acd3e
joshuajenkins109 <joje3221@colorado.edu>
2019-07-05 23:16:07 -0600

    Started PongGamePage, PongBall

---
+++ web/PongBall.js
@@ -0,0 +1,21 @@
+
+
+class PongBall extends React.Component {
+    /*
+        TODO: state -> ball speed, ball modifier, ect.
+        TODO: Render
+     */
+    constructor(props){
+        super(props);
+        this.state = {
+            active: false,
+            startingPlayer: 1,
+
+        }
+
+    }
+
+    render() {
+
+    }
+}
